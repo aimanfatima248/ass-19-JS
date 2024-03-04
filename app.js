55bd4b0bdac5d5c62ae28b257502ccca4b892f9c
aimanfatima248 <aimanfatima65@outlook.com>
2024-03-04 14:16:10 +0500

    add ass 19

---
+++ app.js
@@ -0,0 +1,109 @@
+// CHAP 14-16
+// QS1
+// var arr=[];
+// QS2
+// var names=["",""]
+// // QS3
+// var names=["Aiman","Noor","Saba"];
+// console.log(names)
+// QS4
+// var no=[5,8,4,0];
+// console.log(no)
+// QS5
+// var boolean=[true,false];
+// console.log(boolean)
+// QS6
+// var mixed=[5,"aiman",true,"5"];
+// console.log(mixed)
+// QS7
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write(qualifications)
+// QS8
+// var names=["Michael","John","Tony"];
+// var score=[320,230,480];
+// var totalscore=[500];
+// var percmichael=[320/totalscore*100]
+// var percJohn=[230/totalscore*100]
+// var percTony=[480/totalscore*100]
+// document.write( "Score of Michael is 320"+ " "+ "Percentage:"+ percmichael+ "%" + "<br>" )
+// document.write( "Score of John is 230"+ " "+ "Percentage:"+ percJohn + "%"+  "<br>")
+// document.write( "Score of John is 480"+ " "+ "Percentage:"+ percTony + "%"+  "<br>")
+// QS9
+var colors = ["pink", "purple", "Blue"];
+
+// Displaying the original array
+document.write("<h2>Original Array:</h2>");
+document.write("<p>" + colors.join(", ") + "</p>");
+// a
+// Asking the user for a color to add to the beginning
+// var newColor = prompt("Enter a color to add to the beginning of the array:");
+
+// Adding the new color to the beginning of the array
+// colors.unshift(newColor);
+
+// Displaying the updated array
+// document.write("<h2>Updated Array:</h2>");
+// document.write("<p>" + colors.join(", ") + "</p>");
+// b
+// Asking the user for a color to add to the end
+// var newColor = prompt("Enter a color to add to  end of the array:");
+
+// Adding the new color to the end of the array
+// colors.push(newColor);
+
+// Displaying the updated array
+// document.write("<h2>Updated Array:</h2>");
+// document.write("<p>" + colors.join(", ") + "</p>");
+
+// c
+// Asking the user for a color to add two colors to the beginning
+// var newColor = prompt("Enter a color to add to the beginning of the array:");
+// var newColor2 = prompt("Enter a color to add to the beginning of the array:");
+
+
+// Adding the new color to the beginning of the array
+// colors.unshift(newColor,newColor2);
+
+
+
+// Displaying the updated array
+// document.write("<h2>Updated Array:</h2>");
+// document.write("<p>" + colors.join(", ") + "</p>");
+
+// d
+// removing the first color to the beginning of the array
+// colors.shift ();
+
+
+
+// Displaying the updated array
+// document.write("<h2>Updated Array:</h2>");
+// document.write("<p>" + colors.join(", ") + "</p>");
+// e
+// removing the last color
+// colors.pop ();
+
+
+
+// Displaying the updated array
+// document.write("<h2>Updated Array:</h2>");
+// document.write("<p>" + colors.join(", ") + "</p>");
+// f
+// Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+
+// g
+// var index = +prompt("Enter the index at which you want to add a color:");
+// var colorName = prompt("Enter the color name:");
+
+// colors.splice(index, 1, colorName);
+
+
+// document.write("Updated Array: " + colors);
+h
+
+
+
+
